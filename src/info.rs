3eeb56cec498191b935ae78fb25e4c9c0e602a2b
T. Okubo <t.okubo.rx78+devel@gmail.com>
2023-10-07 21:15:03 +0900

    Add settlement text

--- src/info.rs
+++ src/info.rs
@@ -3,6 +3,9 @@ use crate::planet::Structure;
 pub fn structure_info(structure: &Structure) -> String {
     match structure {
         Structure::None | Structure::Occupied { .. } => unreachable!(),
+        Structure::Settlement { settlement } => {
+            format!("{} ({})", t!("settlement"), t!(settlement.age.as_ref()))
+        }
         _ => {
             t!(structure.kind().as_ref())
         }
