493e6c6c6d464dc531fe4f91b1de3b3bca6385a0
T. Okubo <t.okubo.rx78+devel@gmail.com>
2025-01-07 18:37:46 +0900

    Use modal for saveload window

--- src/ui/saveload.rs
+++ src/ui/saveload.rs
@@ -57,70 +57,61 @@ pub fn show_saveload_window(
         *save_file_list = Some(load_save_file_list());
     }
 
-    egui::Window::new(action_name.clone())
-        .open(open_state)
-        .vscroll(false)
-        .collapsible(false)
-        .resizable([false, false])
-        .default_width(100.0)
-        .anchor(egui::Align2::CENTER_CENTER, [0.0, 0.0])
-        .show(ctx, |ui| {
-            let row_height = egui::TextStyle::Body.resolve(ui.style()).size * 1.2;
-            let table = egui_extras::TableBuilder::new(ui)
-                .striped(true)
-                .resizable(false)
-                .scroll_bar_visibility(egui::scroll_area::ScrollBarVisibility::AlwaysVisible)
-                .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-                .column(egui_extras::Column::auto().at_least(35.0))
-                .column(egui_extras::Column::auto().at_least(180.0))
-                .column(egui_extras::Column::auto().at_least(50.0))
-                .min_scrolled_height(0.0);
+    egui::Modal::new("saveload".into()).show(ctx, |ui| {
+        let row_height = egui::TextStyle::Body.resolve(ui.style()).size * 1.2;
+        let table = egui_extras::TableBuilder::new(ui)
+            .striped(true)
+            .resizable(false)
+            .scroll_bar_visibility(egui::scroll_area::ScrollBarVisibility::AlwaysVisible)
+            .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+            .column(egui_extras::Column::auto().at_least(35.0))
+            .column(egui_extras::Column::auto().at_least(180.0))
+            .column(egui_extras::Column::auto().at_least(50.0))
+            .min_scrolled_height(0.0);
 
-            table
-                .max_scroll_height(220.0)
-                .header(row_height, |mut header| {
-                    header.col(|ui| {
-                        ui.strong("#");
-                    });
-                    header.col(|ui| {
-                        ui.strong(t!("date"));
-                    });
-                    header.col(|_ui| {});
-                })
-                .body(|mut body| {
-                    for i in start..=N_SAVE_FILES {
-                        let time = save_file_list.as_ref().unwrap().saved_time(i);
-                        body.row(row_height, |mut row| {
-                            row.col(|ui| {
-                                if i == 0 {
-                                    ui.label("Auto");
-                                } else {
-                                    ui.label(format!("{}", i));
-                                }
-                            });
-                            row.col(|ui| {
-                                if let Some(time) = time {
-                                    ui.label(time);
-                                }
-                            });
-                            row.col(|ui| {
-                                if (!load || time.is_some())
-                                    && ui.button(action_name.clone()).clicked()
-                                {
-                                    selected = Some(i);
-                                }
-                            });
-                        });
-                    }
+        table
+            .max_scroll_height(220.0)
+            .header(row_height, |mut header| {
+                header.col(|ui| {
+                    ui.strong("#");
                 });
-            ui.add(egui::Separator::default().spacing(2.0).horizontal());
-            ui.vertical_centered(|ui| {
-                if ui.button(t!("cancel")).clicked() {
-                    canceled = true;
+                header.col(|ui| {
+                    ui.strong(t!("date"));
+                });
+                header.col(|_ui| {});
+            })
+            .body(|mut body| {
+                for i in start..=N_SAVE_FILES {
+                    let time = save_file_list.as_ref().unwrap().saved_time(i);
+                    body.row(row_height, |mut row| {
+                        row.col(|ui| {
+                            if i == 0 {
+                                ui.label("Auto");
+                            } else {
+                                ui.label(format!("{}", i));
+                            }
+                        });
+                        row.col(|ui| {
+                            if let Some(time) = time {
+                                ui.label(time);
+                            }
+                        });
+                        row.col(|ui| {
+                            if (!load || time.is_some()) && ui.button(action_name.clone()).clicked()
+                            {
+                                selected = Some(i);
+                            }
+                        });
+                    });
                 }
             });
-        })
-        .unwrap();
+        ui.add(egui::Separator::default().spacing(2.0).horizontal());
+        ui.vertical_centered(|ui| {
+            if ui.button(t!("cancel")).clicked() {
+                canceled = true;
+            }
+        });
+    });
 
     if canceled {
         *open_state = false;
