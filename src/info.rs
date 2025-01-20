216f833142e5caf5ea836b13f38c70190a1d7890
T. Okubo <t.okubo.rx78+devel@gmail.com>
2025-01-20 19:57:37 +0900

    Display large settlement as city

--- src/info.rs
+++ src/info.rs
@@ -3,7 +3,12 @@ use crate::planet::Structure;
 pub fn structure_info(structure: &Structure) -> String {
     match structure {
         Structure::Settlement(settlement) => {
-            format!("{} ({})", t!("settlement"), t!(settlement.age.as_ref()))
+            let s = if settlement.pop >= 10.0 {
+                t!("city")
+            } else {
+                t!("settlement")
+            };
+            format!("{} ({})", s, t!(settlement.age.as_ref()))
         }
         _ => {
             t!(structure.kind().as_ref())
