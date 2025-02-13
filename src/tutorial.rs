5f5f1d3612844026098af4d1d65e6352ce5f9e1b
T. Okubo <t.okubo.rx78+devel@gmail.com>
2025-02-13 18:03:50 +0900

    Add tutorial step

--- src/tutorial.rs
+++ src/tutorial.rs
@@ -5,19 +5,25 @@ use crate::{planet::Planet, sim::SaveState};
 
 pub const TUTORIAL_PLANET: &str = "tutorial";
 
-#[derive(
-    Clone, Copy, PartialEq, Eq, Debug, Serialize, Deserialize, strum::EnumDiscriminants, Resource,
-)]
-pub enum TutorialState {
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Serialize, Deserialize, Resource)]
+pub struct TutorialState {
+    current: TutorialStep,
+    checked: bool,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Serialize, Deserialize, strum::EnumDiscriminants)]
+pub enum TutorialStep {
     Start(usize),
-    TryingMapMove,
-    DescFertile,
-    WaitingGrasslandSpawn,
+    Fertilize,
+    GenOxygen(usize),
 }
 
 impl Default for TutorialState {
     fn default() -> Self {
-        Self::Start(0)
+        Self {
+            current: TutorialStep::Start(0),
+            checked: false,
+        }
     }
 }
 
@@ -25,21 +31,35 @@ pub fn update_tutorial(mut save_state: ResMut<SaveState>, planet: Res<Planet>) {
     let Some(tutorial_state) = &mut save_state.save_file_metadata.tutorial_state else {
         return;
     };
-    if tutorial_state.check_complete(&planet) {
-        *tutorial_state = tutorial_state.next().unwrap();
+    if !tutorial_state.checked && tutorial_state.current.check_complete(&planet) {
+        tutorial_state.checked = true;
     }
 }
 
 impl TutorialState {
-    const ORDER: &[Self] = &[
-        Self::Start(0),
-        Self::Start(1),
-        Self::TryingMapMove,
-        Self::DescFertile,
-        Self::WaitingGrasslandSpawn,
-    ];
+    pub fn move_next(&mut self) {
+        self.current = self.current.next().unwrap();
+        log::info!("change tutorial step to {:?}", self.current);
+    }
 
-    pub fn next(&self) -> Option<Self> {
+    pub fn move_back(&mut self) {
+        self.current = self.current.back().unwrap();
+        log::info!("change tutorial step to {:?}", self.current);
+    }
+
+    pub fn current_step(&self) -> TutorialStep {
+        self.current
+    }
+
+    pub fn checked(&self) -> bool {
+        self.checked
+    }
+}
+
+impl TutorialStep {
+    const ORDER: &[Self] = &[Self::Start(0), Self::Start(1), Self::Fertilize];
+
+    fn next(&self) -> Option<Self> {
         if let Some((i, _)) = Self::ORDER
             .iter()
             .enumerate()
@@ -51,24 +71,55 @@ impl TutorialState {
         }
     }
 
-    pub fn next_by_manual(&self) -> bool {
-        matches!(*self, Self::TryingMapMove)
+    fn back(&self) -> Option<Self> {
+        if let Some((i, _)) = Self::ORDER
+            .iter()
+            .enumerate()
+            .find(|(_, value)| self == *value)
+        {
+            if i > 0 {
+                Self::ORDER.get(i - 1).copied()
+            } else {
+                None
+            }
+        } else {
+            None
+        }
     }
 
-    pub fn has_next_popup_page(&self) -> bool {
-        let d: TutorialStateDiscriminants = self.into();
-        let next: TutorialStateDiscriminants = if let Some(next) = self.next() {
+    pub fn has_next_tutorial(&self) -> bool {
+        let d: TutorialStepDiscriminants = self.into();
+        let next: TutorialStepDiscriminants = if let Some(next) = self.next() {
             next.into()
         } else {
             return false;
         };
-        d == next
+        d != next
     }
 
-    pub fn check_complete(&self, _planet: &Planet) -> bool {
+    pub fn can_back(&self) -> bool {
+        let d: TutorialStepDiscriminants = self.into();
+        let back: TutorialStepDiscriminants = if let Some(back) = self.back() {
+            back.into()
+        } else {
+            return false;
+        };
+        d == back
+    }
+
+    fn check_complete(&self, planet: &Planet) -> bool {
         match *self {
-            Self::WaitingGrasslandSpawn => false,
-            _ => false,
+            Self::Fertilize => check_fertilize(planet),
+            Self::GenOxygen(1) => check_gen_oxygen(planet),
+            _ => true,
         }
     }
+}
+
+fn check_fertilize(_planet: &Planet) -> bool {
+    false
+}
+
+fn check_gen_oxygen(_planet: &Planet) -> bool {
+    false
 }
--- src/ui/tutorial.rs
+++ src/ui/tutorial.rs
@@ -17,75 +17,55 @@ pub fn tutorial_popup(
         return;
     };
 
-    if let Some(popup_ui) = tutorial_state.popup_ui() {
-        occupied_screen_space.opening_modal = true;
+    let has_next_tutorial = tutorial_state.current_step().has_next_tutorial();
+    let can_back = tutorial_state.current_step().can_back();
+    let tutorial_ui = tutorial_state.current_step().ui();
+    let checked = tutorial_state.checked();
 
-        egui::Modal::new("tutorial_popup".into()).show(egui_ctxs.ctx_mut(), |ui| {
-            ui.set_max_width(WINDOW_WIDTH);
-            popup_ui(ui, &textures);
+    let rect = egui::Window::new(t!("tutorial"))
+        .default_width(WINDOW_WIDTH)
+        .show(egui_ctxs.ctx_mut(), |ui| {
+            tutorial_ui(ui, &textures);
             ui.separator();
 
             ui.vertical_centered(|ui| {
-                if tutorial_state.has_next_popup_page() {
-                    if ui.button(t!("next")).clicked() {
-                        *tutorial_state = tutorial_state.next().unwrap();
-                    }
-                } else if ui.button(t!("ok")).clicked() {
-                    *tutorial_state = tutorial_state.next().unwrap();
+                if can_back && ui.button(t!("back")).clicked() {
+                    tutorial_state.move_back();
+                }
+                let s = if has_next_tutorial {
+                    t!("next-tutorial")
+                } else {
+                    t!("next")
+                };
+                if ui.add_enabled(checked, egui::Button::new(s)).clicked() {
+                    tutorial_state.move_next();
                 }
             });
-        });
-    } else if let Some(tutorial_ui) = tutorial_state.tutorial_ui() {
-        let next_by_manual = tutorial_state.next_by_manual();
-
-        let rect = egui::Window::new(t!("tutorial"))
-            .default_width(WINDOW_WIDTH)
-            .show(egui_ctxs.ctx_mut(), |ui| {
-                tutorial_ui(ui, &textures);
-                ui.separator();
-
-                if next_by_manual {
-                    ui.vertical_centered(|ui| {
-                        if ui.button(t!("next-tutorial")).clicked() {
-                            *tutorial_state = tutorial_state.next().unwrap();
-                        }
-                    });
-                }
-            })
-            .unwrap()
-            .response
-            .rect;
-        occupied_screen_space.push_egui_window_rect(rect);
-    }
+        })
+        .unwrap()
+        .response
+        .rect;
+    occupied_screen_space.push_egui_window_rect(rect);
 }
 
-impl TutorialState {
-    fn popup_ui(&self) -> Option<fn(&mut egui::Ui, &EguiTextures)> {
+impl TutorialStep {
+    fn ui(&self) -> fn(&mut egui::Ui, &EguiTextures) {
         match *self {
-            Self::Start(0) => Some(|ui, _| {
+            Self::Start(0) => |ui, _| {
                 ui.label(t!("tutorial", "start-0"));
-            }),
-            Self::Start(1) => Some(start_last),
-            _ => None,
+            },
+            Self::Start(1) => |ui, textures| {
+                ui.label(t!("tutorial", "start-1-1"));
+                ui.vertical_centered(|ui| {
+                    ui.image(textures.get("ui/tutorial-move-keys"));
+                });
+                ui.add_space(8.0);
+                ui.label(t!("tutorial", "start-1-2"));
+                ui.vertical_centered(|ui| {
+                    ui.image(textures.get("ui/icon-map"));
+                });
+            },
+            _ => unreachable!(),
         }
     }
-
-    fn tutorial_ui(&self) -> Option<fn(&mut egui::Ui, &EguiTextures)> {
-        match *self {
-            Self::TryingMapMove => Some(start_last),
-            _ => None,
-        }
-    }
-}
-
-fn start_last(ui: &mut egui::Ui, textures: &EguiTextures) {
-    ui.label(t!("tutorial", "start-1-1"));
-    ui.vertical_centered(|ui| {
-        ui.image(textures.get("ui/tutorial-move-keys"));
-    });
-    ui.add_space(8.0);
-    ui.label(t!("tutorial", "start-1-2"));
-    ui.vertical_centered(|ui| {
-        ui.image(textures.get("ui/icon-map"));
-    });
 }
