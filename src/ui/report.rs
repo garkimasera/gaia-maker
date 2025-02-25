5572d786afa89830e8bf59b86ef7727aa6404594
T. Okubo <t.okubo.rx78+devel@gmail.com>
2025-02-25 17:49:33 +0900

    Add focus button in report

--- src/ui/report.rs
+++ src/ui/report.rs
@@ -2,7 +2,11 @@ use bevy::prelude::*;
 use bevy_egui::{EguiContexts, egui};
 use geom::Coords;
 
-use crate::{conf::Conf, manage_planet::StartEvent, planet::Planet, screen::OccupiedScreenSpace};
+use crate::{
+    conf::Conf,
+    planet::{Planet, TILE_SIZE},
+    screen::{Centering, OccupiedScreenSpace},
+};
 
 use super::{Dialog, WindowsOpenState};
 
@@ -71,7 +75,7 @@ pub fn report_windows(
     mut egui_ctxs: EguiContexts,
     mut occupied_screen_space: ResMut<OccupiedScreenSpace>,
     mut wos: ResMut<WindowsOpenState>,
-    mut _ew_start_event: EventWriter<StartEvent>,
+    mut ew_centering: EventWriter<Centering>,
 ) {
     let mut close_dialogs = Vec::new();
     for (i, dialog) in wos.dialogs.iter_mut().enumerate() {
@@ -81,10 +85,10 @@ pub fn report_windows(
         let dialog = match dialog {
             Dialog::Report(report_ui) => egui::Window::new(&report_ui.head)
                 .open(&mut open)
+                .default_height(240.0)
                 .vscroll(true)
                 .show(egui_ctxs.ctx_mut(), |ui| {
-                    ui.label(&report_ui.body);
-                    if let Some(_pos) = report_ui.pos {}
+                    report_ui.ui(ui, &mut ew_centering);
                 }),
             _ => unreachable!(),
         };
@@ -97,5 +101,19 @@ pub fn report_windows(
 
     for &i in close_dialogs.iter().rev() {
         wos.dialogs.remove(i);
+    }
+}
+
+impl ReportUi {
+    fn ui(&self, ui: &mut egui::Ui, ew_centering: &mut EventWriter<Centering>) {
+        ui.label(&self.body);
+        if let Some(pos) = self.pos {
+            if ui.button(t!("focus")).clicked() {
+                ew_centering.send(Centering(Vec2::new(
+                    pos.0 as f32 * TILE_SIZE,
+                    pos.1 as f32 * TILE_SIZE,
+                )));
+            }
+        }
     }
 }
